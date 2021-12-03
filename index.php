3ac420fe1fb2b510ace0205833c51c5554d45fd5
mboy1011 <lyjieme@gmail.com>
2021-12-03 16:31:34 +0800

    dded HTML5 & CSS

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<!DOCTYP html>
+<html>
+<head>
+	<title>Web Application</title>
+	<style>
+		body {
+			background-color:#fafafa;
+		}
+	</style>
+</head>
+<body>
 <?PHP
 	echo "Hello, GCCians!";
 	echo "<h1>Welcome to Git!";
 ?>
+</body>
